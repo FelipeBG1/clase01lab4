0134d246b64e41cc4d27da4f26049a49315aa5fc
FelipeBG1 <felibustosomg@gmail.com>
2021-08-26 12:40:08 -0300

    heroku deploy

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'PrimerProyecto';
+  edadUno : number = 0;
+  edadDos : number = 0;
+  promedio : number = 0;
+  suma : number = 0;
+
+  calcular()
+  {
+    this.promedio = (this.edadUno + this.edadDos) / 2;
+    this.suma = this.edadUno + this.edadDos;
+  }
+
+  limpiar()
+  {
+    this.edadUno = 0;
+    this.edadDos = 0;
+    this.promedio = 0;
+    this.suma = 0;
+  }
+}
