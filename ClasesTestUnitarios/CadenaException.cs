0c89f48a3529e16a847f2149884de58d75a3e374
Felipe Bustos Gil <60651449+FelipeBG1@users.noreply.github.com>
2023-05-22 16:15:52 -0300

    Prueba
    asdasd

---
+++ ClasesTestUnitarios/CadenaException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesTestUnitarios
+{
+    public class CadenaException : Exception
+    {
+        public CadenaException(string mensaje) : base(mensaje)
+        {
+
+        }
+    }
+}
---
+++ ClasesTestUnitarios/Prueba.cs
@@ -0,0 +1,44 @@
+﻿namespace ClasesTestUnitarios
+{
+    public class Prueba
+    {
+        private int numero;
+        private string cadena;
+
+        public int Numero { get { return this.numero; } set { this.numero = value; } }
+        
+        public string Cadena { get { return this.cadena; } set { this.ValidarCadena(value); } }
+
+        public int AumentarNumero(int valor)
+        {
+            this.numero += valor;
+
+            return this.numero;
+        }
+
+        public void ValidarCadena(string cadena)
+        {
+            if(cadena == "" || cadena.Length <= 3)
+            {
+                throw new CadenaException("La cadena es muy corta para asignar");
+            }
+
+            this.cadena = cadena;
+        }
+
+        public static bool operator ==(Prueba p, string cadena)
+        {
+            if(p.cadena == cadena)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool operator !=(Prueba p, string cadena)
+        {
+            return !(p == cadena);
+        }
+    }
+}
